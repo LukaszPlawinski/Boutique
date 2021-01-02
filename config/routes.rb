fa00ae7636c7596cd15fcb329fa5beb8a1880298
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2021-01-02 08:32:12 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/home'
-  get 'static_pages/thankyou'
+  get '/' => 'static_pages#home'
+  get '/thankyou' => 'static_pages#thankyou'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
