bcf9d13274ceb271d7b41392cbd4414db0f3ddc3
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2021-01-02 14:08:24 +0000

    Created Cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cart/index'
   resources :products
   get '/' => 'static_pages#home'
   get '/thankyou' => 'static_pages#thankyou'
   get '/login' => 'user#login'
   get '/logout' => 'user#logout'
+  get '/cart', to: 'cart#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
