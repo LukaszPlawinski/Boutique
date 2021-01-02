8ea42426a9b5c74941bb7421b60052330f891417
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2021-01-02 16:47:31 +0000

    Devise installed and user model added to database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  
+  #Devise configuration
+  config.action_mailer.default_url_options = { :host => '127.0.0.1:3000'}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -74,4 +77,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'cart/index'
   resources :products
   get '/' => 'static_pages#home'
