4812019175c5bca2ada6ebc8e8866c43e6974dc5
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2021-01-03 12:25:03 +0000

    changed default value in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_113448) do
+ActiveRecord::Schema.define(version: 2021_01_03_122119) do
 
   create_table "categories", charset: "latin1", force: :cascade do |t|
     t.string "title"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2021_01_03_113448) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
